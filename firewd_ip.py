08b0604e717d369a3c40a75797ff3efefb67fc33
pokerstarxy <857607332@qq.com>
2018-05-24 18:10:50 +0800

    update

--- firewd_ip.py
+++ firewd_ip.py
@@ -7,48 +7,48 @@ from flask.ext.cache import Cache
 from logging import getLogger
 from logging import Formatter
 app = Flask(__name__)
-app.config.update(
-    DEBUG=True,
-    MAIL_SERVER='smtp.sina.com',
-    MAIL_PROT=25,
-    MAIL_USE_TLS = True,
-    MAIL_USE_SSL = False,
-    MAIL_USERNAME = 'test_code@sina.com',
-    MAIL_PASSWORD = 'pwd@test',
-    MAIL_DEBUG = True
-)
-
-config = {
-    'CACHE_TYPE': 'redis',
-    'CACHE_REDIS_HOST': '127.0.0.1',
-    'CACHE_REDIS_PORT': 6379,
-    'CACHE_REDIS_DB': '',
-    'CACHE_REDIS_PASSWORD': ''
-}
-app.config.from_object(config)
-cache.init_app(app)
-ADMINS = ['yourname@example.com']
-if not app.debug:
-    mail_handler = SMTPHandler('127.0.0.1',
-                               'server-error@example.com',
-                               ADMINS, 'YourApplication Failed')
-    mail_handler.setLevel(logging.ERROR)
-    mail_handler.setFormatter(Formatter('''
-    Message type:       %(levelname)s
-    Location:           %(pathname)s:%(lineno)d
-    Module:             %(module)s
-    Function:           %(funcName)s
-    Time:               %(asctime)s
-
-    Message:
-
-    %(message)s
-    '''))
-    app.logger.addHandler(mail_handler)
-    loggers = [app.logger, getLogger('sqlalchemy'),
-               getLogger('mail')]
-    for logger in loggers:
-        logger.addHandler(mail_handler)
+# app.config.update(
+#     DEBUG=True,
+#     MAIL_SERVER='smtp.sina.com',
+#     MAIL_PROT=25,
+#     MAIL_USE_TLS = True,
+#     MAIL_USE_SSL = False,
+#     MAIL_USERNAME = 'test_code@sina.com',
+#     MAIL_PASSWORD = 'pwd@test',
+#     MAIL_DEBUG = True
+# )
+#
+# config = {
+#     'CACHE_TYPE': 'redis',
+#     'CACHE_REDIS_HOST': '127.0.0.1',
+#     'CACHE_REDIS_PORT': 6379,
+#     'CACHE_REDIS_DB': '',
+#     'CACHE_REDIS_PASSWORD': ''
+# }
+# app.config.from_object(config)
+# cache.init_app(app)
+# ADMINS = ['yourname@example.com']
+# if not app.debug:
+#     mail_handler = SMTPHandler('127.0.0.1',
+#                                'server-error@example.com',
+#                                ADMINS, 'YourApplication Failed')
+#     mail_handler.setLevel(logging.ERROR)
+#     mail_handler.setFormatter(Formatter('''
+#     Message type:       %(levelname)s
+#     Location:           %(pathname)s:%(lineno)d
+#     Module:             %(module)s
+#     Function:           %(funcName)s
+#     Time:               %(asctime)s
+#
+#     Message:
+#
+#     %(message)s
+#     '''))
+#     app.logger.addHandler(mail_handler)
+#     loggers = [app.logger, getLogger('sqlalchemy'),
+#                getLogger('mail')]
+#     for logger in loggers:
+#         logger.addHandler(mail_handler)
 
 
 @app.route('/')
--- models.py
+++ models.py
@@ -11,7 +11,7 @@
 from firewd_ip import  app
 from flask_sqlalchemy import  SQLAlchemy
 #sql 配置
-app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:171024@127.0.0.1/ipfire'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:asdasd@192.168.0.7/chenwm_test'
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
 db=SQLAlchemy(app)
 
@@ -19,61 +19,78 @@ db=SQLAlchemy(app)
 class VISIT(db.Model):
     __tablename__ = 'ip_visit_log'
     id=db.Column(db.Integer, primary_key=True,autoincrement=True)
-    partno=db.Column(db.string(30),)
-    query_time=db.Column(db.datetime,default=datetime.now)
-    # ip_detail_id=db.Column(db.Integer,db.ForeignKey())
-    ipinfo(ip_detail_id)=
+    partno=db.Column(db.VARCHAR(30),)
+    query_time=db.Column(db.DATETIME,default=datetime.now)
+    ip_detail_id=db.Column(db.Integer, db.ForeignKey('ip_info.id'))
 
-    def __init__(self,id,partno,query_time,ip_detail_id):
-        self.id=id
-        self.partno=partno
-        self.query_time=query_time
-        self.ip_detail_id=ip_detail_id
 
+    # def __init__(self,id,partno,query_time,ip_detail_id):
+    #     self.id=id
+    #     self.partno=partno
+    #     self.query_time=query_time
+    #     self.ip_detail_id=ip_detail_id
+    def __repr__(self):
+        return  self.partno
 
 class IPINFO(db.Model):
     __tablename__='ip_info'
     id=db.Column(db.Integer, primary_key=True,autoincrement=True)
-    ip=db.Column(db.string(16),nullable=False)
-    logname=db.Column(db.string(11),)
-    ip_log=db.Column(db.string(32),index=True,unique=True,nullable=False)
-    unlock_1m_times=
-    unlock_30m_times=
-    (AREA)area_id=
-    today_times=
-    total_times=
-    lock_status=
-    white_list_status=
-    unlock_times=
-    create_time=
-    lastest_time=
+    ip=db.Column(db.VARCHAR(16),nullable=False)
+    logname=db.Column(db.VARCHAR(11),)
+    ip_log=db.Column(db.CHAR(32),index=True,unique=True,nullable=False)
+    lock_1m_times=db.Column(db.VARCHAR(4))
+    lock_30m_times=db.Column(db.VARCHAR(4))
+    area_id=db.Column(db.Integer, db.ForeignKey('ip_code_info.id'))
+    today_times=db.Column(db.Integer,default=0)
+    total_times =db.Column(db.Integer,default=0)
+    lock_status=db.Column(db.BOOLEAN,default=0)
+    white_list_status=db.Column(db.SMALLINT,default=0)
+    unlock_times=db.Column(db.Integer,default=0)
+    create_time=db.Column(db.DATETIME,default=datetime.now)
+    lastest_time=db.Column(db.DATETIME,default=datetime.now)
+    ip_detail_id = db.relationship('VISIT', backref='ip_detail',lazy='dynamic')
+
+    def __repr__(self):
+        return  self.ip
 
 
 class UNLOCk(db.Model):
     __tablename__='ip_rate_limit'
-    id=
-    time_value=
-    rate_1m=
+    id=db.Column(db.Integer, primary_key=True,autoincrement=True)
+    time_value=db.Column(db.CHAR(3))
+    rate_1m=db.Column(db.VARCHAR(4))
     # rate_10m=
-    rate_30m=
+    rate_30m=db.Column(db.VARCHAR(4))
     # rate_60m=
-    rate_ipseg=
+    rate_ipseg=db.Column(db.VARCHAR(6))
+
+    def __repr__(self):
+        return  self.__tablename__
 
 
 class AREA(db.Model):
     __tablename__='ip_code_info'
-    id=
-    country_code=
-    city_code=
-    unlock(arealimit_id)=
+    id=db.Column(db.Integer, primary_key=True,autoincrement=True)
+    country_code=db.Column(db.VARCHAR(3))
+    city_code=db.Column(db.VARCHAR(4))
+    area_limit_id = db.relationship('IPINFO', backref='ip_area', lazy='dynamic')
+
+    def __repr__(self):
+        return  self.country_code
 
 
 class IPSEG(db.Model):
     __tablename__='ip_ipseg_info'
-    id=
-    ip=
-    ipseg(md5)=
-    ipseg_status=
-    create_time=
+    id=db.Column(db.Integer, primary_key=True,autoincrement=True)
+    ip=db.Column(db.VARCHAR(12))
+    ipseg(md5)=db.Column(db.CHAR(32))
+    ipseg_status=db.Column(db.SMALLINT)
+    create_time=db.Column(db.DATETIME,default=datetime.now)
+
+    def __repr__(self):
+        return  self.ip
 
 
+
+
+db.create_all()
